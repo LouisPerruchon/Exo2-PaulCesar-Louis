54683795a818c6e50f620b234df61dd187cb2271
Paul C <paulcesar75.pct@gmail.com>
2023-09-25 17:03:08 +0200

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GestionComponent } from './gestion/gestion.component';
+import { ListeFilmComponent } from './liste-film/liste-film.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path : "/Accueil", component : AppComponent 
+},
+  {
+  path : "/Gestion", component : GestionComponent 
+},
+  {
+  path : "/Liste Film", component : ListeFilmComponent 
+},
+{
+  path : "/Contact", component : ContactFormComponent 
+},
+{
+  path :'**', component : AppComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatMenuModule} from '@angular/material/menu';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
