fd4630ed33aa8fa25846b674cf7f3e68b84ccc8c
Paul C <paulcesar75.pct@gmail.com>
2023-09-25 16:36:49 +0200

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListeFilmComponent } from './liste-film/liste-film.component';
+import { FilmDetailComponent } from './liste-film/film-detail/film-detail.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { GestionComponent } from './gestion/gestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ListeFilmComponent,
+    FilmDetailComponent,
+    ContactFormComponent,
+    GestionComponent
   ],
   imports: [
     BrowserModule,
